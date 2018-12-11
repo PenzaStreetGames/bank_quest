e1e36fe8b8d09775efc237b0691629b674638a3c
Gamba232 <parassitn@mail.ru>
2018-12-11 23:57:41 +0300

    Начало работы над построением системы

--- Bank Quest.py
+++ Bank Quest.py
@@ -8,21 +8,28 @@ class Quest:
         self.player = name
         self.rooms = {}
         self.ways = {}
-        room_names, room_text, ways = self.load_data()
-        images = ""
-        for i in range(len(room_names)):
-            room = Room(room_names[str(i)], room_text[str(i)],
-                        images[str[i]], i)
-            self.rooms[str(i)] = room
-        way_items = ways.items()
-        for i in range(len(ways)):
-            room_from, room_into = way_items[i][0].split()
 
         self.buttons = [canvas.choose_1, canvas.choose_2,
                         canvas.choose_3, canvas.choose_4]
 
     def change_room(self, index):
         pass
+
+    def create_rooms(self):
+        self.rooms = {}
+        room_names, room_text, ways = self.load_data()
+        images = ""
+        for i in range(len(room_names)):
+            room = Room(room_names[str(i)], room_text[str(i)],
+                        images[str[i]], i)
+            self.rooms[str(i)] = room
+
+    def create_ways(self):
+        self.ways = {}
+        way_items = ways.items()
+        for i in range(len(ways)):
+            way_name = way_items[0]
+            room_from_name, room_into_name = way_items[i][0].split()
 
 
 class Room:
@@ -36,7 +43,7 @@ class Room:
 
 class Hall:
 
-    def __init__(self, room_from, room_to, condition, index):
+    def __init__(self, room_from, room_to, name, text):
         self.room_from = room_from
         self.room_to = room_to
         self.condition = condition
