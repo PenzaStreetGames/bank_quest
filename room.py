e7559f9f1cba9fdf3c0d321ac5cc383c4d793348
medal <webformedal@gmail.com>
2018-12-11 19:13:12 +0300

    Интерфейс 0.3. Новая графика. Доработаны функции управления, музыка непрерывна, выход из программы, начаты разработки флагов.

--- room.py
+++ room.py
@@ -1,45 +1,58 @@
 import sys
 import pygame
-from time import sleep
 from PyQt5.QtCore import QSize, QStringListModel
 from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QPlainTextEdit, QVBoxLayout
 from PyQt5.QtWidgets import QMainWindow, QTableWidget, QWidget, QLineEdit
+from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.Qt import QHBoxLayout, QSpacerItem, QSizePolicy
 
-class MyWidget(QMainWindow):
+
+class SceneInterface(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(300, 300, 800, 500)
-        self.setWindowTitle('Комната')
+
+        self.bg = QLabel(self)
+        self.bg.move(0, 0)
+        self.bg.setPixmap(QPixmap("bg.jpg"))
+        self.bg.resize(800, 500)
 
         self.exit = QPushButton('Выход', self)
-        self.exit.resize(115, 40)
-        self.exit.move(665, 455)
+        self.exit.resize(105, 40)
+        self.exit.move(675, 455)
+        self.exit.setFont(QFont("PSG Font", 11))
+        self.exit.clicked.connect(self.close)
 
         self.restart = QPushButton('Рестарт', self)
-        self.restart.resize(120, 40)
-        self.restart.move(540, 455)
+        self.restart.resize(105, 40)
+        self.restart.move(560, 455)
+        self.restart.setFont(QFont("PSG Font", 11))
 
         self.text = QPlainTextEdit(self)
         self.text.move(20, 20)
-        self.text.resize(510, 270)
+        self.text.resize(530, 270)
         self.text.setReadOnly(True)
+        self.text.setFont(QFont("PSG Font", 10))
+        self.text.setStyleSheet("background: rgba(255, 255, 255, 0.9);")
 
         self.player_data = QPlainTextEdit(self)
-        self.player_data.move(540, 240)
-        self.player_data.resize(240, 190)
+        self.player_data.move(560, 300)
+        self.player_data.resize(220, 130)
         self.player_data.setReadOnly(True)
-
+        self.player_data.setFont(QFont("PSG Font", 10))
+        self.player_data.setStyleSheet("background: rgba(238, 238, 238, 0.97);\
+                                        border:none;")
 
         self.name_player = QLineEdit(self)
-        self.name_player.resize(240, 30)
-        self.name_player.move(540, 205)
-        self.name_player.setReadOnly(True)
+        self.name_player.resize(220, 44)
+        self.name_player.move(560, 245)
+        self.name_player.setFont(QFont("PSG Font", 11))
+        self.name_player.setStyleSheet("background: rgba(255, 255, 255, 0.90);\
+                                        border:none;\
+                                        padding-left: 5px;")
 
-        self.label = QLabel(self)
-        self.label.setText("Картинка")
-        self.label.resize(200, 160)
-        self.label.move(700, 30)
+        self.img = QLabel(self)
+        self.img.resize(220, 220)
+        self.img.move(560, 20)
 
         self.layout = QVBoxLayout(self)
 
@@ -47,19 +60,20 @@ class MyWidget(QMainWindow):
         self.btn_layout.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum))
         self.layout.addLayout(self.btn_layout)
 
-
     def initButtons(self, names):
         for btn in range(len(names)):
             button = QPushButton(names[btn], self)
             self.btn_layout.addWidget(button)
             button.move(20, (45 * btn) + 300)
-            button.resize(510, 40)
+            button.resize(530, 40)
+            button.setFont(QFont("PSG Font", 11))
+            button.setStyleSheet("background: rgba(236, 236, 236, 0.7);")
 
     def initText(self, text):
         self.text.setPlainText(text)
 
     def initImage(self, image):
-        pass
+        self.img.setPixmap(QPixmap(image))
 
     def initPlayerData(self, data):
         self.player_data.setPlainText(data)
@@ -70,18 +84,25 @@ class MyWidget(QMainWindow):
     def initNamePlayer(self, name):
         self.name_player.setText(name)
 
+    def setNameDisabled(self):
+        self.name_player.setReadOnly(True)
+
+
 app = QApplication(sys.argv)
-ex = MyWidget()
+ex = SceneInterface()
+ex.setFixedSize(800, 500)
 
 ex.initNameScene("Scene")
 ex.initText("This is simple text forever")
 ex.initButtons(["Вариант 1", "Вариант 2", "Вариант 3"])
 ex.initNamePlayer("user123")
+ex.initImage("img.jpg")
 ex.initPlayerData("DataPlayer")
+ex.setNameDisabled()
 
 pygame.mixer.init()
 pygame.mixer.music.load('Quest Theme.mp3')
-pygame.mixer.music.play()
+pygame.mixer.music.play(-1)
 
 ex.show()
 sys.exit(app.exec_())
